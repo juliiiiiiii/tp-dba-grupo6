d3b94083437f6945fbfc6bfb681aa6b6918c63e9
nahueldebellis <debellisnahuel@gmail.com>
2026-06-11 23:17:19 -0300

    add empresa + SP

---
+++ src/Empresa.sql
@@ -0,0 +1,20 @@
+use parque_nacional;
+
+create table Concesiones.Empresa (
+	id int not null primary key identity(1, 1),
+	nombre varchar(25),
+	tipo varchar(100),
+	cuit varchar(15) check(cuit like '3[034][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]' or cuit like '2[0347][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]')
+);
+
+create or alter procedure Concesiones.AltaEmpresa (@nombre varchar(25), @tipo varchar(100), @cuit int) as begin
+	insert into Concesiones.Empresa (nombre, tipo, cuit) values (@nombre, @tipo, @cuit)
+end;
+
+create or alter procedure Concesiones.BajaEmpresa (@id int) as begin
+	delete from Concesiones.Empresa where id=@id
+end;
+
+create or alter procedure Concesiones.ModificacionEmpresa (@id int, @nombre varchar(25) = null, @tipo varchar(100) = null, @cuit int = null) as begin
+	update Concesiones.Empresa set nombre=isnull(@nombre, nombre), tipo=isnull(@tipo, tipo), cuit=isnull(@cuit, cuit) where id = @id
+end;
